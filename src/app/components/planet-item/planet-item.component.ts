5834ecd0143e9b7aafdb856033342f292796285b
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2019-02-22 17:03:01 +0100

    Responsive styling Planet Item and Planets Components

--- src/app/components/planet-item/planet-item.component.ts
+++ src/app/components/planet-item/planet-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Planet } from "./../../../models/Planet";
 import { ActivatedRoute } from "@angular/router";
 import { PlanetService } from "./../../services/planet.service";
+import { Location } from "@angular/common";
 
 @Component({
   selector: "app-planet-item",
@@ -10,14 +11,28 @@ import { PlanetService } from "./../../services/planet.service";
 })
 export class PlanetItemComponent implements OnInit {
   planet: Planet;
+  // residents: any;
+
+  // checkResidents() {
+  //   if (!this.planet.residents) {
+  //     this.residents = this.planet.residents.push("Nothing");
+  //     console.log(this.residents);
+  //   }
+
+  //   return this.residents;
+  // }
+
+  // films: string[] = this.planet.films;
 
   constructor(
     private route: ActivatedRoute,
-    private planetService: PlanetService
+    private planetService: PlanetService,
+    private location: Location
   ) {}
 
   ngOnInit() {
     this.getPlanet();
+    // this.checkResidents();
   }
 
   //  Pass the name as id param and show the component with data
@@ -27,5 +42,8 @@ export class PlanetItemComponent implements OnInit {
     this.planetService
       .getPlanet(name)
       .subscribe(planet => (this.planet = planet));
+  }
+  goBack(): void {
+    this.location.back();
   }
 }
